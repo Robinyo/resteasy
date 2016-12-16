6e28c514283f650a0d03d4b92952e5cd5183e7c5
Rob Ferguson <rob.ferguson@robferguson.org>
2016-12-16 11:21:44 +1100

    Updated the README.md files

---
+++ examples/qbuddy-api/src/main/java/io/qbuddy/api/QBuddyApplication.java
@@ -0,0 +1,49 @@
+package io.qbuddy.api;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import io.qbuddy.api.resource.MessageResource;
+import io.swagger.jaxrs.config.BeanConfig;
+
+public class QBuddyApplication extends Application {
+
+    HashSet<Object> singletons = new HashSet<Object>();
+
+    public QBuddyApplication() {
+        configureSwagger();
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        HashSet<Class<?>> set = new HashSet<Class<?>>();
+
+        set.add(MessageResource.class);
+
+        set.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        set.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+
+        return set;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+
+    private void configureSwagger() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setSchemes(new String[] { "http" });
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/api");
+        beanConfig.setResourcePackage(MessageResource.class.getPackage().getName());
+        beanConfig.setTitle("QBuddy RESTful API");
+        beanConfig.setDescription("QBuddy RESTful API built using RESTEasy, Swagger and Swagger UI "
+                + "packaged in a Fat JAR with an embedded Jetty instance and no web.xml.");
+        beanConfig.setScan(true);
+    }
+}
